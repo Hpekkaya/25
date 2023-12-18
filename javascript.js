6309801998b4d4b28a0ac6b0fec20d356f8f61f1
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-18 17:49:15 +0300

    For loop tried

---
+++ javascript.js
@@ -0,0 +1,73 @@
+// const cars = ["BMW","Volvo","Ford"]
+// let i = 0 ;
+// let len = cars.length ;
+// let text = "";
+// for (; i < len ; i++){
+//     // text = text + cars[i] + "\n"
+//     text += cars[i] + "\r\n"
+//     console.log(i)
+// }
+// console.log(text)
+
+// const arr = [1,2,3,4,5,6]
+
+// for(let l = 0, r= arr.length - 1 ; l < r ; l++ , r--) {
+//     console.log(arr[l],arr[r])
+// }
+ 
+// for in statement loops through the properties of an Object, array as well:
+
+// const person = {
+//     FName: "John",LName:"Day",Age:25
+// }
+// let text = ""
+// for(let x in person){
+//     console.log(x," :",person[x])
+//     text += x + " :" + person[x] + "\n\n" 
+// }
+// console.log(text)
+ 
+// const number = [45,4,9,40]
+// for(let x in number){
+//     console.log(x, number[x])
+// }
+
+// const number = ["45","4","9","40"]
+// // console.log(typeof null)
+// let text = ""
+// let text1 = 0
+// number.forEach(myFunction)
+
+
+// function myFunction(value,index,array){
+//     console.log("index:", index)
+//     console.log("value:", value)
+//     console.log("array", array)
+//     text += value
+//     text1 += Number(value)
+// }
+// console.log("Text added each other as a string     : ", text)
+// console.log("Text added after to Number each other : ", text1)
+ 
+// let language = "Javascript"
+// let text = ""
+// for (let x in language){
+//     console.log(x,language[x])
+// }
+
+// const cars = ["Volvo","Fiat","Mercedes"]
+// console.log(cars)
+// // const cars = "Mercedes"
+// // console.log(cars)
+// // const cars = {model:"mercedes"} //objeler için for in kullanılır
+// console.log(cars)
+// let text = ""
+// for(let x of cars){
+//     console.log(x)
+//     text += x + "-"
+// }
+// console.log(text)
+ 
+
+
+
