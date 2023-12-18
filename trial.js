f5107a40bac2820ff103f00c950d17fc7bc1e4d7
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-18 18:53:06 +0300

    this keywors tried

--- trial.js
+++ trial.js
@@ -1,3 +1,17 @@
 
 
+//this kullanımı
+
+// console.log(this)
+// Window.alert("bu bir uyarıdır")
+
+const person = {
+    name: "musa",
+    description: function (){
+        return this.name;
+    }
+}
+console.log(person.description())
+
+
 
