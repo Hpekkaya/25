077ae1c506584f387ecdbba0b551add843da4cf6
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-20 19:08:35 +0300

    this repeated

--- javascript.js
+++ javascript.js
@@ -150,6 +150,130 @@
 // }
 // console.log(text)
 
+// const numbers = [45,23,34,67]
+// let txt=""
+// for(let x in numbers){
+//     console.log("Value of variable x as       :",x)
+//     console.log("Type of variable x           :", typeof(x))
+//     console.log("Value of variable numbers[x] :",numbers[x])
+//     console.log("__________________________",x)
+// }
+// // for in tabiri key yada index değerlerini döndürür
+// numbers.forEach(myFunction)
+// function myFunction (value,index,array){
+//     console.log(value,index,array)
+// }
+// let language = "Javascript"
+// for (let x in language){
+//     console.log(x)
+//     console.log(language[x])
+// }
+
+// //for of (returns values)
+// const cars = ["fiat","opel","bmw"]
+// for (let x of cars){
+//     console.log("Value",x)
+// }
+ 
+// let language = "javascript"
+// for (let x of language) {
+//     console.log(x)
+// }
+
+// If we aimed to reach object values, we should use for in
+// For in specialized for objects
+// Below loop returns error : TypeError: person is not iterable
+// let person = {fname:"ahmet", lname:"can"}
+// for(let x of person){
+//     console.log(x)
+// }
+
+//while //First control the condition then execute the loop
+// let i = 99 ;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// //do-while //execute the loop before control the condition 
+// //          First execute the loop, control the condition at the end 
+// let j = 99 ;
+// do {
+//     console.log(j);
+//     j++;
+// }
+// while (j < 10)  
+
+
+// //break ve continue
+// //Use in the loop
+// //break (end the loop)
+// //continue do not execute the rest of the loop and continue loop with the increase loop variable
+
+// for(let i = 0 ; i < 10 ; i++){
+//     if(i === 3) {
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// for(let i = 0 ; i < 10 ; i++){
+//     if(i === 3 || i === 7) {
+//         console.log("hatalı değer bulundu")
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// try-catch-finally error management tools
+
+// try {
+//     console.log("mesaj içeriği sağlıklı")
+// } 
+// catch(error){
+//     console.log(error.message)
+// }
+
+// let x = "  ";
+// try {
+//     if(x.trim() == "") throw {message:"boş içerik hatası"}
+// }
+// catch(err){
+//     console.log(err.message)
+// }
+// finally {
+//     console.log("try catch içeriği sonlandı.")
+// }
+
+// let x = "18"
+// try {
+//     x = Number(x);
+//     if(x<5) throw "çok küçük, sayı 5 den büyük olmalıdır"
+//     if(x>10) throw "çok büyük, sayı 10 dan küçük olmalı"
+//     console.log(x)
+// }
+// catch (err) {
+//     console.log(err)
+// }
+// finally {
+//     console.log("kod bloğu hatasız çalıştı")
+// }
+
+//this kullanımı
+// console.log(this)
+// const person = {
+//     name: "mustafa",
+//     fname:"önal",
+//     description: function () {
+//         return this;
+//     },
+//     showName: function () {
+//         return this.name
+//     }
+// }
+// console.log(person.description())
+// console.log(person.showName())
 
 
 
