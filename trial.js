02ff5a16293925505686902d6865a2b22b0a8f83
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-22 12:21:46 +0300

    this and constructor tried.

--- trial.js
+++ trial.js
@@ -1,15 +1,25 @@
-// 
-const person = {
-  name : "Mustafa",
-  sname : "Onal",
-  descrption :function(){
-    return this;
-  },
-  showName :function(){
-    let x = this.name + " " + this.sname
-    return  x    
-    
+// Constructor with function trial
+
+
+function Person(name,yOB,job, year){
+  this.name = name;
+  this.yOB = yOB;
+  this.job = job;
+  this.year = year; 
+  
+  this.age = function() {       
+    return  this.year - this.yOB
   }
 }
-console.log(person.descrption())
-console.log(person.showName())
+
+let hakan = new Person('Hakan', 1970, 'retired',);
+let alev = new Person('Alev', 1975, 'Teacher');
+
+let d = new Date();
+let year = d.getFullYear();
+console.log(year)
+
+console.log(hakan.name, "  ",hakan.yOB, " ", hakan.job, " ", hakan.age())
+console.log(alev.name, "  ",alev.yOB, " ", alev.job, " ", alev.age())
+
+
