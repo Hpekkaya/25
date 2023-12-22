94443c0db119e64678855adf67c374e56487f243
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-22 10:04:55 +0300

    try catch repeated

--- trial.js
+++ trial.js
@@ -1,2 +1,13 @@
-
-
+let x=11;
+try {
+  x = Number(x)
+  if(x < 5) throw "The number is too small, must bigger then"
+  if(x >10)  throw "The number is too big, must smaller then"
+  console.log(x)
+} 
+catch(error){
+  console.log(error)
+}
+finally {
+  console.log("Try catch ended")
+}
